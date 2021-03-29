94190bc80c21e708ea106185e85325e3d490adac
Liju Jose <liju.jose@gds.ey.com>
2021-03-29 12:54:59 +0530

    fix: security vulnerabilities resolved

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,11 @@
 module.exports = {
-  extends: ['codfish', 'codfish/docker', 'codfish/dapp'],
+  extends: [require.resolve('eslint-config-codfish')].filter(Boolean),
   root: true,
   rules: {
     'no-console': 'off',
     'no-plusplus': 'off',
     'no-bitwise': 'off',
-    'no-restricted-syntax': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        'devDependencies': ['integration-test/test.js', 'integration-test/testData.js'],
-      },
-    ],
+    'no-restricted-syntax': 'off'
   },
   env: {
     mocha: true,
